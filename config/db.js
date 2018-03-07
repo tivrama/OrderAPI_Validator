8f0e750f2949075dd37c16f706fcafcb48a6a6fe
tivrama <joelschoolniknarvar>
2018-03-06 20:22:20 -0800

    Change db to prod

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ module.exports = {
   // INSERT YOUR DB URL PARAMETERS HERE
 
   // url to prod DB (Uncomment for production/go-live)
-  // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds143241.mlab.com:43241/orderapi'
+  url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds143241.mlab.com:43241/orderapi'
 
   // url to testing DB (Comment out when going live)
-  url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds143241.mlab.com:43241/orderapi-dev'
+  // url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds143241.mlab.com:43241/orderapi-dev'
 };
